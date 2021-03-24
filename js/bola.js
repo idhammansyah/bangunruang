2889f7fdd214a955d5d73a51750f42daaf69af28
Idham Mansyah Awwalu <59715715+idhammansyah@users.noreply.github.com>
2021-03-24 21:03:44 +0700

    Add files via upload

---
+++ js/bola.js
@@ -0,0 +1,7 @@
+function cariluas() {
+    jari = formulir.jari.value;
+    jari = formulir.jari.value;
+    jari = formulir.jari.value;
+    formulir.luas.value = 4 * 3.14 * jari * jari;
+    formulir.volume.value = 1.3333333 * 3.14 * jari * jari * jari;
+}
